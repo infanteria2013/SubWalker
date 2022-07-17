1cf7f2f2e6575073dc71471bb0c52c4f221c8618
Jesús Bermúdez <50809468+jesusmariabermudez@users.noreply.github.com>
2022-07-17 10:43:09 -0500

    Update subwalker.sh

--- subwalker.sh
+++ subwalker.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
-# m8r0wn
+# m8r0wn Principal
+# editado por jmbsvene
 
 TARGET=$1
 if ! [ $TARGET ]; then
@@ -29,14 +30,25 @@ cd $TOOLS_DIR/assetfinder
 echo "[*] Launching assetfinder"
 ./assetfinder --subs-only $TARGET > $OUT_DIR/assetfinder.txt &
 
+cd $TOOLS_DIR/subzzZ
+echo "[*] Lauching subzzZ"
+./subzzZ -d $TARGET ; cd scans/$TARGET; cp root_subdomains.txt $OUT_DIR/subzzZ.txt &
+
 echo "[*] Waiting until all scripts complete..."
 wait
 
 cd $OUT_DIR
-(cat subscraper.txt sublist3r.txt assetfinder.txt | sort -u) > subwalker.txt
-rm subscraper.txt sublist3r.txt assetfinder.txt
+(cat subscraper.txt sublist3r.txt assetfinder.txt  subzzZ.txt | sort -u) > subwalker.txt ; cat subwalker.txt | httpx -ports 80,443,81,8443,8080,8000,10000,9000 -silent -threads 1000 | tee -a alive-$TARGET.txt
+
+rm subscraper.txt sublist3r.txt assetfinder.txt subzzZ.txt
+
 
 RES=$(cat subwalker.txt | wc -l)
+RES-alive=$(cat alive-$TARGET.txt | wc -l)
 echo -e "\n[+] SubWalker complete with ${RES} results"
+echo -e "\n[+] SubWalker complete with ${RES-alive} results"
+
 echo "[+] Output saved to: $OUT_DIR/subwalker.txt"
+echo "[+] Output saved to: $OUT_DIR/alive-$TARGET.txt"
+
 exit 0
